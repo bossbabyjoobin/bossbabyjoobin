a6eb046fc4d4c50a5fd73c220f67da073549e01b
Mark Comia <joobinsfromm@gmail.com>
2024-12-02 15:00:58 +0800

    Quartz sync: Dec 2, 2024, 3:00 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -92,16 +92,6 @@ const config: QuartzConfig = {
       Plugin.NotFoundPage(),
     ],
   },
-export interface FullPageLayout {
-  head: QuartzComponent // single component
-  header: QuartzComponent[] // laid out horizontally
-  beforeBody: QuartzComponent[] // laid out vertically
-  pageBody: QuartzComponent // single component
-  afterBody: QuartzComponent[] // laid out vertically
-  left: QuartzComponent[] // vertical on desktop and tablet, horizontal on mobile
-  right: QuartzComponent[] // vertical on desktop, horizontal on tablet and mobile
-  footer: QuartzComponent // single component
-}
 }
 
 export default config
