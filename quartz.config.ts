92ac04d49a158e02e86f1e51dcb1b25ef388231c
Mark Comia <joobinsfromm@gmail.com>
2024-12-02 23:53:57 +0800

    Quartz sync: Dec 2, 2024, 11:53 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: " (∩^o^)⊃☆🐣☀️🦭",
+    pageTitle: "jb.sh.kt❧",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
