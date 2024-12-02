3932085efe457231177d3b9012d1825421aefb4d
Mark Comia <joobinsfromm@gmail.com>
2024-12-03 00:24:30 +0800

    Quartz sync: Dec 3, 2024, 12:24 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,7 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.For JooBin, SeoAh and Kotone🎔}{" "}
+          {i18n(cfg.locale).components.footer.createdWith}{"For JooBin, SeoAh and Kotone🎔"}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
