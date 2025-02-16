40f6cb97f83c8f9d52aa39b1400425fea752ab22
Mark Comia <joobinsfromm@gmail.com>
2025-02-16 21:19:18 +0800

    Quartz sync: Feb 16, 2025, 9:19 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,8 +19,7 @@ const config: QuartzConfig = {
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: false,
-	defaultSocialImage: "og-image.png",
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
